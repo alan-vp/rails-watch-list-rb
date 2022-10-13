bca7ac998fb85882905c8ea0af30559b10167c1f
alan vergara <alan_vergara_p@hotmail.com>
2022-10-13 18:18:19 -0500

    pass all model test and create API seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies = URI.open(url).read
+response = JSON.parse(movies)
+
+top_10_movies = response["results"].first(10)
+
+top_10_movies.each do |movie|
+  movie_hash =  {title: movie["title"], overview: movie['overview'], poster_url:"https://image.tmdb.org/t/p/w500#{movie['poster_path']}", rating:movie['vote_average']}
+  Movie.create(movie_hash)
+end
