099a9051900310c5fa245008103c93bdc0be064a
alan vergara <alan_vergara_p@hotmail.com>
2022-10-18 16:01:49 -0500

    design on cards from index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ url = 'https://tmdb.lewagon.com/movie/top_rated'
 movies = URI.open(url).read
 response = JSON.parse(movies)
 
-top_10_movies = response["results"].first(10)
+top_10_movies = response['results'].first(10)
 
 top_10_movies.each do |movie|
-  movie_hash =  {title: movie["title"], overview: movie['overview'], poster_url:"https://image.tmdb.org/t/p/w500#{movie['poster_path']}", rating:movie['vote_average']}
+  movie_hash =  {title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/w200#{movie['poster_path']}", rating:movie['vote_average']}
   Movie.create(movie_hash)
 end
