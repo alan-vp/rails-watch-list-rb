da906bcfc47046259af91ec39ca2facbcc5c7a15
alan vergara <alan_vergara_p@hotmail.com>
2022-10-25 13:30:59 -0500

    change ruby version to be compatible with heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@ response = JSON.parse(movies)
 top_10_movies = response['results'].first(10)
 
 top_10_movies.each do |movie|
-  movie_hash =  {title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/w200#{movie['poster_path']}", rating:movie['vote_average']}
+  movie_hash = { title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/w200#{movie['poster_path']}", rating: movie['vote_average'] }
   Movie.create(movie_hash)
 end
